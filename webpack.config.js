e70b0863e63ca802a3abfc8af24bff6aecad3bf3
Christian König <christian@2strong.de>
2020-07-25 17:46:41 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const pugFiles = fs
+  .readdirSync(path.resolve(__dirname, 'src'))
+  .filter((filename) => filename.endsWith('.pug'));
+
+module.exports = {
+  entry: {
+    app: './src/js/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].bundle.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './src/images', to: 'images' }],
+    }),
+    ...pugFiles.map(
+      (filename) =>
+        new HtmlWebpackPlugin({
+          filename: filename.replace('.pug', '.html'),
+          template: `./src/${filename}`,
+        })
+    ),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: ['pug-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              output: './dist/css/',
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
