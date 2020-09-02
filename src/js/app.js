b372a0d7cbb246036f3b2156df8cd1178dc0fd47
Christian König <christian@2strong.de>
2020-09-02 17:56:35 +0200

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,24 @@
 import '../styles/main.scss';
+
+const form = document.querySelector('form');
+
+if (form) {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let email = '';
+
+    Array.from(form.elements).forEach((formElement) => {
+      if (
+        formElement.value &&
+        formElement.type !== 'submit' &&
+        formElement.type !== 'reset'
+      ) {
+        email += `${formElement.name}:%20${formElement.value}%0a`;
+      }
+    });
+
+    location.href =
+      'mailto:wolf.usingen@t-online.de?subject=Antwort%20ueber%20Ferienwohnungen-Frankfurt.com&body=Ihre%20Daten:%0a%0a' +
+      email;
+  });
+}
